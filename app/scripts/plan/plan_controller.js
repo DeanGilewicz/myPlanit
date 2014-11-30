f5a29c8239d10fd0d492e3c9f129fcc5611595fe
Dean Gilewicz <gilewiczd@gmail.com>
2014-11-29 22:16:04 -0500

    added delete function

--- app/scripts/plan/plan_controller.js
+++ app/scripts/plan/plan_controller.js
@@ -3,22 +3,23 @@
     .controller('PlanCtrl', ['PlanFactory', '$scope', '$rootScope', '$location',
       function (PlanFactory, $scope, $rootScope, $location) {
 
+        PlanFactory.getPois().success( function (results) {
+          console.log(results);
+          $scope.poiList = results.results;
+        });
+
         $scope.addPoi = function (poi) {
           PlanFactory.addPoi(poi);
           // $rootScope.$on('poi:added', function () {
-          //   $location.path('/');
+          //   $location.path('/plan');
           // });
         }
 
-
-        PlanFactory.getPois().then( function (results) {
-          console.log(results);
-          $scope.poiList = results.data.results;
-        });
-
-
-
-
+        $scope.deletePoi = function (poiID, index) {
+          PlanFactory.deletePoi(poiID).success( function () {
+            $scope.poiList.splice(index, 1);
+          });
+        };
 
 
       }
--- app/scripts/plan/plan_factory.js
+++ app/scripts/plan/plan_factory.js
@@ -1,26 +1,31 @@
 (function () {
   angular.module('myPlanit')
-    .factory('PlanFactory', ['PARSE_HEADERS', 'PARSE_URI', '$http', '$location',
-      function (PARSE_HEADERS, PARSE_URI, $http, $location) {
+    .factory('PlanFactory', ['PARSE_HEADERS', 'PARSE_URI', '$http', '$rootScope', '$location',
+      function (PARSE_HEADERS, PARSE_URI, $http, $rootScope, $location) {
+
+        var getPois = function () {
+          return $http.get(PARSE_URI + 'classes/PoiList', PARSE_HEADERS);
+        }
 
         // add a poi to "plan" list
         var addPoi = function (poi) {
           $http.post(PARSE_URI + 'classes/PoiList', poi, PARSE_HEADERS).success( function (data) {
             console.log(data);
           });
-          //   .then( function () {
+          // .then( function () {
           //   // broadcast to the parent controller that the poi has been added
-          //   $rootScope.$broadcast('appt:added');
+          //   $rootScope.$broadcast('poi:added');
           // });
         }
 
-        var getPois = function (poi) {
-          return $http.get(PARSE_URI + 'classes/PoiList', PARSE_HEADERS);
-        }
+        var deletePoi = function (poiID) {
+          return $http.delete(PARSE_URI + 'classes/PoiList/' + poiID, PARSE_HEADERS);
+        };
 
         return {
+          getPois: getPois,
           addPoi: addPoi,
-          getPois: getPois
+          deletePoi: deletePoi
         }
 
       }
