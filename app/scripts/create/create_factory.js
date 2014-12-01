80bdaf7258bfab6616235cee0c965d49405b3c2b
Dean Gilewicz <gilewiczd@gmail.com>
2014-12-01 16:23:27 -0500

    added comments to create html

--- app/scripts/create/create_factory.js
+++ app/scripts/create/create_factory.js
@@ -6,13 +6,17 @@
 
         // add a new Plan Object as an array
         function createPlan (plan) {
+          // create array
           var planArray = [];
+          // add name from form into array
           planArray.push(plan.name);
 
+          // create a Parse Object that will be table header
           var dbObject = {
+            // create property: value to be able to store it in this way on Parse
             plans: planArray
           }
-
+          // save new array to Parse
           $http.post(PARSE_URI + 'classes/Plans', dbObject, PARSE_HEADERS).success( function (data) {
             console.log(data);
           });
