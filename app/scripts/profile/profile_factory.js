a6ad1fc484d95237288e026d94e1857941b51191
Dean Gilewicz <gilewiczd@gmail.com>
2014-12-03 20:07:21 -0500

    plans for specific user working in profile

--- app/scripts/profile/profile_factory.js
+++ app/scripts/profile/profile_factory.js
@@ -1,15 +1,28 @@
 (function () {
   angular.module('myPlanit')
-    .factory('ProfileFactory', ['PARSE_URI', 'PARSE_HEADERS', '$http',
-      function (PARSE_URI, PARSE_HEADERS, $http) {
+    .factory('ProfileFactory', ['PARSE_URI', 'PARSE_HEADERS', '$http', '$cookieStore',
+      function (PARSE_URI, PARSE_HEADERS, $http, $cookieStore) {
 
         // get all plans for specific user
         var plansByUser = function () {
+
+          var currentUser = $cookieStore.get('currentUser');
+          console.log(currentUser);
+
+          var params = '?where={"user":{"__type":"Pointer","className":"_User","objectId":"'+ currentUser.objectId +'"}}';
+
+          return $http.get(PARSE_URI + 'classes/Plans/' + params, PARSE_HEADERS);
+
+
+          // var currentUser = $cookieStore.get('currentUser');
+          // var userPlans = $cookieStore.get('Plans.user');
+          // console.log(currentUser);
+          // console.log(userPlans);
           // queries the plan id and compares to user
-          // var query = '?'+'where={"User": {"$select": {"query": {"className": "Plans", "where": {"pois": {"user"}';
+          // var query = '?'+'where={"currentUser":"'userPlans.objectId+'"}';
           // return $http.get(PARSE_URI + 'classes/Plans' + query, PARSE_HEADERS);
 
-            return $http.get(PARSE_URI + 'classes/Plans', PARSE_HEADERS);
+            // return $http.get(PARSE_URI + 'classes/Plans', PARSE_HEADERS);
         }
 
 
